cf22d6ebbd1cbf31369a393eaeb8bdd7c09e0f94
dr-Maker <contacto@jonathanvenegas.cl>
2022-05-30 22:37:05 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports =
+{
+
+	entry: 
+    {
+    	app: "./src/index.js"
+    },
+
+	output: 
+    {
+    	path: path.resolve(__dirname, "dist"),
+    	filename: "bundle.js",
+    	publicPath: "./"
+    },
+
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+
+	plugins:
+        [
+        	new HtmlWebpackPlugin({
+        		template: "src/index.html"
+        	})
+        ],
+
+	module:
+        {
+        	rules:
+            [
+            	{
+            		test: /\.js$/,
+            		exclude: /node_modules/,
+            		use: {
+            			loader: "babel-loader",
+            			options: {
+            				presets: [
+            					"@babel/preset-env",
+            					"@babel/preset-react"
+            				]
+            			}
+            		}
+            	}
+            ]
+        },
+	devServer: 
+        {
+        	historyApiFallback: {
+        		disableDotRule: true
+        	},
+        	liveReload: true,   
+        	open: true,
+        	port: 8000
+        },
+};
